a7fed8b76a19e3c6bebcc39bbd8a127abbc9f38f
Charlie Briggs <3199181+Limess@users.noreply.github.com>
2020-06-09 19:34:25 +0100

    Add log message indicating which stream failed to load (#59)

--- target_redshift/__init__.py
+++ target_redshift/__init__.py
@@ -345,17 +345,20 @@ def flush_streams(
 
 
 def load_stream_batch(stream, records_to_load, row_count, db_sync, delete_rows=False, compression=None, slices=None, temp_dir=None):
-    # Load into redshift
-    if row_count[stream] > 0:
-        flush_records(stream, records_to_load, row_count[stream], db_sync, compression, slices, temp_dir)
+    # Load into redshift                                            
+    try:
+        if row_count[stream] > 0:
+            flush_records(stream, records_to_load, row_count[stream], db_sync, compression, slices, temp_dir)
 
-        # Delete soft-deleted, flagged rows - where _sdc_deleted at is not null
-        if delete_rows:
-            db_sync.delete_rows(stream)
+            # Delete soft-deleted, flagged rows - where _sdc_deleted at is not null
+            if delete_rows:
+                db_sync.delete_rows(stream)
 
-        # reset row count for the current stream
-        row_count[stream] = 0
-
+            # reset row count for the current stream
+            row_count[stream] = 0
+    except Exception as e:
+        LOGGER.exception("Failed to load stream %s to Redshift", stream)
+        raise e
 
 def chunk_iterable(iterable, size):
     """Yield successive n-sized chunks from iterable. The last chunk is not padded"""
