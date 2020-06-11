db5ffe4eac293880852c00588b9af47d3dbf55ca
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2020-06-11 17:50:57 +0100

    Switch to Draft7Validator (#61)

--- target_redshift/__init__.py
+++ target_redshift/__init__.py
@@ -13,7 +13,7 @@ from decimal import Decimal
 from tempfile import mkstemp
 
 from joblib import Parallel, delayed, parallel_backend
-from jsonschema import Draft4Validator, FormatChecker
+from jsonschema import Draft7Validator, FormatChecker
 from singer import get_logger
 from itertools import islice
 
@@ -204,7 +204,7 @@ def persist_lines(config, lines, table_cache=None) -> None:
             stream = o['stream']
 
             schemas[stream] = float_to_decimal(o['schema'])
-            validators[stream] = Draft4Validator(schemas[stream], format_checker=FormatChecker())
+            validators[stream] = Draft7Validator(schemas[stream], format_checker=FormatChecker())
 
             # flush records from previous stream SCHEMA
             # if same stream has been encountered again, it means the schema might have been altered
