cebcf2f6dee132776f960f6c422543df26139e3d
Labhamm Gupta <labhamm@users.noreply.github.com>
2023-08-03 01:18:51 +0530

    feat: Update __init__.py

--- target_redshift/__init__.py
+++ target_redshift/__init__.py
@@ -229,7 +229,7 @@ def persist_lines(config, lines, table_cache=None) -> None:
             #  2) Use fastsync [postgres-to-redshift, mysql-to-redshift, etc.]
             if config.get('primary_key_required', True) and len(o['key_properties']) == 0:
                 LOGGER.critical("Primary key is set to mandatory but not defined in the [{}] stream".format(stream))
-                raise Exception("key_properties field is required")
+                raise Exception("UPDTEED: key_properties field is required")
 
             key_properties[stream] = o['key_properties']
 
