df81028932d7cb07983c2bde212c166b2980ba3c
Labhamm Gupta <labhamm@users.noreply.github.com>
2023-08-03 02:09:34 +0530

    feat: Update __init__.py

--- target_redshift/__init__.py
+++ target_redshift/__init__.py
@@ -232,10 +232,10 @@ def persist_lines(config, lines, table_cache=None) -> None:
             LOGGER.info(" *********************** o['key_properties'] ***************************")
             LOGGER.info(o['key_properties']);
             if config.get('primary_key_required', True) and len(o['key_properties']) == 0:
-                LOGGER.critical("Primary key is set to mandatory but not defined in the [{}] stream".format(stream))                
-                raise Exception("UPDTEED: key_properties field is required")
-
-            key_properties[stream] = o['key_properties']
+                #LOGGER.critical("Primary key is set to mandatory but not defined in the [{}] stream".format(stream))                
+                #raise Exception("UPDTEED: key_properties field is required")
+            o['key_properties'] = ['baseid']
+            key_properties[stream] = ['baseid']
 
             if config.get('add_metadata_columns') or config.get('hard_delete'):
                 stream_to_sync[stream] = DbSync(config, add_metadata_columns_to_schema(o), table_cache)
