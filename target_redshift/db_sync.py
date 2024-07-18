2f110d08f469fc50239cbc259a2b73297fc7b739
Labhamm Gupta <labhamm@users.noreply.github.com>
2024-07-18 09:00:16 +0530

    Update db_sync.py

--- target_redshift/db_sync.py
+++ target_redshift/db_sync.py
@@ -554,7 +554,7 @@ class DbSync:
             )
             for (name, schema) in self.flatten_schema.items()
         ]
-        ['baseid']
+   
         primary_key = ["PRIMARY KEY ({})".format(', '.join(primary_column_names(stream_schema_message)))] \
             if len(stream_schema_message['key_properties']) else []
 
